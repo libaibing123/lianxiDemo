73e2b47e8f229c1af8e2a63984a8886f8b46d8aa
libaibing123 <309183559@qq.com>
2018-06-04 18:20:46 +0900

    第一次上传

--- app/src/main/java/com/example/lianxidemo/MainActivity.java
+++ app/src/main/java/com/example/lianxidemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Button(this);
+        //第一次看看能不能传明白
     }
 }
